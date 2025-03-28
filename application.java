729afa751d1bce78f868e563a3049a6588eded32
ravireddy1994 <ravichandra4196@gmail.com>
2025-03-28 12:21:22 +0530

    feature 201 changes

--- application.java
+++ application.java
@@ -4,3 +4,18 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+
+#feature 201 changes
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Variable declaration
+        int x = 1;
+
+        // While loop
+        while (x <= 5) {
+            System.out.println("Welcome to Java Programming: " + x);
+            x++;
+        }
+    }
+}
